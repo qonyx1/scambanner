2fa4e73225877fcb80b91bef8d9ec0aff9d53ed7
qonyx1 <onyxabitch@proton.me>
2025-04-12 04:35:44 +0000

    various fixes

--- frontend/cogs/cases/case_create.py
+++ frontend/cogs/cases/case_create.py
@@ -172,7 +172,8 @@ class ConfirmCancelView(View):
                 processed_proof_links
             )
             review_view = CaseReviewView(self.bot, case_embed, self.accused_id, self.reason, processed_proof_links, responsible_guild, investigator)
-            sent_message = await queue_channel.send(embed=case_embed, view=review_view)
+            role = await interaction.guild.fetch_role(system_config["discord"]["admin_team_role_id"])
+            sent_message = await queue_channel.send(f"{role.mention}",embed=case_embed, view=review_view)
             review_view.message = sent_message
 
 
